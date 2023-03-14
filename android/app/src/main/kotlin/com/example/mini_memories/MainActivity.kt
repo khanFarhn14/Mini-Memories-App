6cbc48788daeff5765f7b404f00a9dd9a7f90905
Khan Farhan <thecubeshade21@gmail.com>
2023-03-14 21:08:54 +0530

    Setup the Flutter file

---
+++ android/app/src/main/kotlin/com/example/mini_memories/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mini_memories
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mini_memories/home.dart';
+
+void main() => runApp
+(
+  const MaterialApp
+  (
+    debugShowCheckedModeBanner: false,
+    home: HomePage()
+  )
+);
